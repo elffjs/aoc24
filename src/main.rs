e28db011536ea6b8ac693a52da76e094c1aefb80
Dylan Moreland <dylan@dimo.zone>
2024-12-01 18:09:43 -0500

    Hit it with cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use std::collections::HashMap;
-use std::io::{BufReader, BufRead};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()> {
-    let file =  File::open("inputs/day1")?;
+    let file = File::open("inputs/day1")?;
     let reader = BufReader::new(file);
 
     let mut left = Vec::new();
     let mut right = Vec::new();
 
-
     for line in reader.lines() {
         let line = line?;
 
-        let numbers: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
+        let numbers: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
 
         left.push(numbers[0]);
         right.push(numbers[1]);
@@ -22,7 +24,11 @@ fn main() -> std::io::Result<()> {
     left.sort_unstable();
     right.sort_unstable();
 
-    let result1: i32 = left.iter().zip(right.iter()).map(|x| (x.0 - x.1).abs()).sum();
+    let result1: i32 = left
+        .iter()
+        .zip(right.iter())
+        .map(|x| (x.0 - x.1).abs())
+        .sum();
 
     println!("Total distance: {}", result1);
 
@@ -32,7 +38,10 @@ fn main() -> std::io::Result<()> {
         *right_counts.entry(x).or_insert(0) += 1;
     }
 
-    let result2: i32 = left.iter().map(|x| x * right_counts.get(x).copied().unwrap_or(0)).sum();
+    let result2: i32 = left
+        .iter()
+        .map(|x| x * right_counts.get(x).copied().unwrap_or(0))
+        .sum();
 
     println!("Similarity score: {}", result2);
 
